b4f52a6a6945124cd12db8c3eb75bc977b57d0e9
Braay5 <luis.b.ryan@hotmail.com>
2022-11-01 18:49:00 -0600

    main
    Se modifico el codigo para cambiar de escena.

--- Juego dos UTECA/Assets/Scripts/MovimientoEscena.cs
+++ Juego dos UTECA/Assets/Scripts/MovimientoEscena.cs
@@ -1,40 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MovimientoEscena : MonoBehaviour
 {
-    public int EscenaActual
+    public void SiguienteEscena()
     {
-        get { return escenaActual; }
-        set
-        {
-            if (value == 5)
-                escenaActual = 1;
-            else if (value == 0)
-                escenaActual == 4;
-            else
-                escenaActual = value;
-        
-        }
-    }
-
-    private int escenaActual;
-    private int escenaPrevia;
-
-    private void Start()
-    {
-        escenaPrevia = 0;
-        escenaActual = 1;
-    }
-
-    private void Update()
-    {
-        if(escenaActual != escenaPrevia)
-        {
-            GetComponent<SpriteRenderer>().sprite = Sprites.Load<Sprite>
-        }
-
-        escenaPrevia = escenaActual;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
